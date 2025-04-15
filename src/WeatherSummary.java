86d3c3b723fa1700e8b1f8d4ce050a38dd6c5d94
Bluenzo <80460870+Bluenzo@users.noreply.github.com>
2025-04-14 22:10:45 -0700

    This is the Wave 4, to print the average

--- src/WeatherSummary.java
+++ src/WeatherSummary.java
@@ -22,19 +22,27 @@ public class WeatherSummary {
         Scanner scanner = new Scanner(System.in);
         double max = Double.NEGATIVE_INFINITY;
         double min = Double.POSITIVE_INFINITY;
+        double sum = 0;
+        int count = 0;
+        double average = 0;
 
         while (scanner.hasNextDouble()) {
             double temp = scanner.nextDouble();
             max = Math.max(max, temp);
             min = Math.min(min, temp);
+            sum += temp;
+            count++;
         }
 
+        //I calculate the average and put the print statement.
+        average = sum / count;
         System.out.println("Max: " + max);
         System.out.println("Min: " + min);
+        System.out.println("Average: " + average);
         
 
 
-        
+
         scanner.close();
     }
 }
